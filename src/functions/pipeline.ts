ac3b662aabf4e85c9de105c56152991ee2a679f7
akthrms <akt.hrms@gmail.com>
2020-06-20 05:11:06 +0900

    update

--- src/functions/pipeline.ts
+++ src/functions/pipeline.ts
@@ -1,15 +1,33 @@
+/**
+ * パイプラインのクラス
+ */
 class Pipeline<A> {
+  /**
+   * コンストラクタ
+   * @param value
+   */
   constructor(private value: A) {}
 
-  pipe<B>(f: (a: A) => B): Pipeline<B> {
+  /**
+   * 関数を適用して、パイプラインをつなげる
+   * @param f
+   */
+  then<B>(f: (a: A) => B): Pipeline<B> {
     return new Pipeline(f(this.value));
   }
 
+  /**
+   * パイプラインを終了して、値を取り出す
+   */
   end(): A {
     return this.value;
   }
 }
 
+/**
+ * パイプラインを開始する
+ * @param value
+ */
 export function start<A>(value: A): Pipeline<A> {
   return new Pipeline(value);
 }
